ed6596be2737c661cd4c77f8fd40cc23316c29c0
ezsultan <sultansetiawan885@gmail.com>
2025-01-09 10:41:19 +0700

    test

--- detect_plate.py
+++ detect_plate.py
@@ -39,8 +39,8 @@ def run(
         source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
+        conf_thres=0.50,  # confidence threshold
+        iou_thres=0.50,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         view_img=False,  # show results
@@ -159,8 +159,6 @@ def run(
 
             # Stream results
             im0 = annotator.result()
-            # cv2.imshow(str(p), im0)
-            # cv2.waitKey(1)
             send_data_to_ws(ws_server, im0, "test")
             if save_img:
                 if dataset.mode == 'image':
